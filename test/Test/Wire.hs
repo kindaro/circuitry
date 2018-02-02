05395d7da3f9451d3400940e2017ee33bc1b7f6b
Ignat Insarov <kindaro@gmail.com>
2018-02-02 22:00:16 +0700

    Cleanup in Test: Remove unused imports.

--- test/Test/Wire.hs
+++ test/Test/Wire.hs
@@ -3,12 +3,7 @@ module Test.Wire where
 import Circuitry
 import Circuitry.Types
 import Circuitry.Misc
-import Circuitry.Backend
 import Control.Monad
-import Diagrams.TwoD.Layout.Constrained
-import Data.ByteString.Char8 as C8
-import System.Directory (getCurrentDirectory)
-import System.FilePath ((</>))
 
 circuit = runCircuit $ void $
   anon bend $ \(in', inp) ->
